8352d97013b896adab0cc7833320005e34fcf775
Oldřich Daněk <oldrich_danek@centrum.cz>
2023-12-22 17:51:34 +0100

    AVL tree

---
+++ AVL_binary_tree.py
@@ -0,0 +1,97 @@
+# implementation of AVL binary tree exercise
+
+# classical Node binary tree class with parameter height which represent leaf-root distance
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+
+
+#  class consists from methods for handling with AVL binary tree
+class AVLTree:
+    def get_height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def get_balance(self, node):
+        if not node:
+            return 0
+        return self.get_height(node.left) - self.get_height(node.right)
+
+    def rotate_right(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        return x
+
+    def rotate_left(self, x):
+        y = x.right
+        T2 = y.left
+
+        y.left = x
+        x.right = T2
+
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+
+        return y
+
+    def insert(self, root, key):
+        if not root:
+            return Node(key)
+
+        if key < root.key:
+            root.left = self.insert(root.left, key)
+        elif key > root.key:
+            root.right = self.insert(root.right, key)
+        else:
+            return root
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+
+        balance = self.get_balance(root)
+
+        if balance > 1 and key < root.left.key:
+            return self.rotate_right(root)
+
+        if balance < -1 and key > root.right.key:
+            return self.rotate_left(root)
+
+        if balance > 1 and key > root.left.key:
+            root.left = self.rotate_left(root.left)
+            return self.rotate_right(root)
+
+        if balance < -1 and key < root.right.key:
+            root.right = self.rotate_right(root.right)
+            return self.rotate_left(root)
+
+        return root
+
+    def print_tree(self, root, level=0, prefix="Root: "):
+        if root:
+            print(" " * (level * 4) + prefix + str(root.key))
+            if root.left or root.right:
+                self.print_tree(root.left, level + 1, "L--- ")
+                self.print_tree(root.right, level + 1, "R--- ")
+
+
+# place of usage
+if __name__ == "__main__":
+
+    avl_tree = AVLTree()
+    numbers_to_insert = [10, 5, 15, 3, 7, 12, 18, 1, 2, 3, 85, 12, 84, 83, 90, 4, 6, 7]
+
+    root = None
+    for number in numbers_to_insert:
+        root = avl_tree.insert(root, number)
+
+    avl_tree.print_tree(root)
