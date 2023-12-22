b0b591643c864d48570c65152b9319b87c3664e8
Oldřich Daněk <oldrich_danek@centrum.cz>
2023-12-22 20:54:54 +0100

    AVL tree

--- AVL_binary_tree.py
+++ AVL_binary_tree.py
@@ -11,6 +11,12 @@ class Node:
 
 #  class consists from methods for handling with AVL binary tree
 class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def get_root(self):
+        return self.root
+
     def get_height(self, node):
         if not node:
             return 0
@@ -20,6 +26,26 @@ class AVLTree:
         if not node:
             return 0
         return self.get_height(node.left) - self.get_height(node.right)
+
+    def balance_node(self, root):
+
+        balance = self.get_balance(root)
+
+        if balance > 1 and self.get_balance(root.left) >= 0:
+            return self.rotate_right(root)
+
+        if balance > 1 and self.get_balance(root.left) < 0:
+            root.left = self.rotate_left(root.left)
+            return self.rotate_right(root)
+
+        if balance < -1 and self.get_balance(root.right) <= 0:
+            return self.rotate_left(root)
+
+        if balance < -1 and self.get_balance(root.right) > 0:
+            root.right = self.rotate_right(root.right)
+            return self.rotate_left(root)
+
+        return root
 
     def rotate_right(self, y):
         x = y.left
@@ -45,35 +71,54 @@ class AVLTree:
 
         return y
 
-    def insert(self, root, key):
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, root, key):
         if not root:
             return Node(key)
 
         if key < root.key:
-            root.left = self.insert(root.left, key)
+            root.left = self._insert(root.left, key)
         elif key > root.key:
-            root.right = self.insert(root.right, key)
+            root.right = self._insert(root.right, key)
         else:
             return root
 
         root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
 
-        balance = self.get_balance(root)
+        return self.balance_node(root)
 
-        if balance > 1 and key < root.left.key:
-            return self.rotate_right(root)
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
 
-        if balance < -1 and key > root.right.key:
-            return self.rotate_left(root)
+    def _delete(self, root, key):
+        if not root:
+            return root
 
-        if balance > 1 and key > root.left.key:
-            root.left = self.rotate_left(root.left)
-            return self.rotate_right(root)
+        if key < root.key:
+            root.left = self._delete(root.left, key)
+        elif key > root.key:
+            root.right = self._delete(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            temp = self._get_min_value_node(root.right)
+            if temp:
+                root.key = temp.key
+                root.right = self._delete(root.right, temp.key)
+            else:
+                return None  # Opraveno z return root.right
 
-        if balance < -1 and key < root.right.key:
-            root.right = self.rotate_right(root.right)
-            return self.rotate_left(root)
+        # Aktualizace výšky a vyvážení uzlu
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        return self.balance_node(root)
 
+    def _get_min_value_node(self, root):
+        while root and root.left:
+            root = root.left
         return root
 
     def print_tree(self, root, level=0, prefix="Root: "):
@@ -87,10 +132,20 @@ class AVLTree:
 # place of usage
 if __name__ == "__main__":
     avl_tree = AVLTree()
-    root = None
+
     while True:
-        new_key = input("Insert new element (or '' to exit):")
-        if new_key == "":
-            break
-        root = avl_tree.insert(root, new_key)
-        avl_tree.print_tree(root)
+        try:
+            new_key = int(input("Insert or delete element (type '0' to exit, and use '-' to delete element):"))
+            if new_key == 0:
+                break
+            elif new_key > 0:
+                root = avl_tree.insert(new_key)
+            elif new_key < 0:
+                root = avl_tree.delete(abs(new_key))
+            else:
+                pass
+
+            avl_tree.print_tree(avl_tree.get_root())
+        except ValueError as ve:
+            print(f"Error: {ve}")
+            pass
