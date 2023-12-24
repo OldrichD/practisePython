d09b8d75882b82e0e074541309019718cbb4508e
Oldřich Daněk <oldrich_danek@centrum.cz>
2023-12-24 16:47:12 +0100

    marry christmas

--- marry_christmas.py
+++ marry_christmas.py
@@ -17,6 +17,18 @@ lines = [
     "*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *",
     "  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *",
 ]
+
+
+def draw_tree(size: int):
+    for i in range(1, size):
+        tree_rows = 8*((size-i)*" "+(2*i-1)*"*"+(size-i)*" ")
+        print(tree_rows)
+    tree_trunks = 8*((size-1)*" "+"|"+(size-1)*" ")
+    print(tree_trunks)
+
+
+draw_tree(5)
+
 pause = 0.5
 for line in lines:
     print(line)
